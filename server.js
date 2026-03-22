99b64ae03b9ba9d80f205d940cd60174845f520c
Astha Singh <asthaasingh011@gmail.com>
2026-03-22 14:42:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mysql = require("mysql"); 
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// 🔗 CONNECT DATABASE
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "CaptureCare11", // or "root" if you set password
+  database: "schemes_db"
+});
+
+// ✅ CHECK CONNECTION
+db.connect(err => {
+  if (err) {
+    console.log("❌ DB connection failed:", err);
+  } else {
+    console.log("✅ Connected to MySQL");
+  }
+});
+
+// 📡 API
+app.get("/schemes", (req, res) => {
+  db.query("SELECT * FROM schemes", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+app.get("/domain-count", (req, res) => {
+  db.query(
+    "SELECT domain, COUNT(*) AS total FROM schemes GROUP BY domain",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+});
+app.listen(3000, () => {
+  console.log("🚀 Server running at http://localhost:3000");
+});
